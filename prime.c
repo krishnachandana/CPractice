b1a175e969a2aa47b2e8718e9fdbaa653e821782
krishnachandana <31621416+krishnachandana@users.noreply.github.com>
2017-09-06 23:31:55 +0530

    prime or not a prime
    input:
    2
    output:
    prime

--- prime.c
+++ prime.c
@@ -1,15 +1,21 @@
 #include<stdio.h>
 int main()
 {
-	int i;
-	scanf("%d",&i);
-	if(i/2==1){
+	int i,n,flag=0;
+	scanf("%d",&n);
+	for(i=2;i<=n/2;i++){
+		if(n%i==0)
+			{
+				flag=1;
+				break;
+			}
+		}
+	if(flag==0)
 		printf("prime");
-	}
-	else if(i/2==0)
-        {
+	
+	else
 		printf("not prime");
-	}
+		printf("\n");
 	return 0;
 }
 	
